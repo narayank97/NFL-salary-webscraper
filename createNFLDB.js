cf7a0f00aa89a79bafc333a0262377158de44a0f
narayank97 <narayankarun@gmail.com>
2020-01-09 18:21:22 -0800

    tried to fix create db

--- createNFLDB.js
+++ createNFLDB.js
@@ -4,29 +4,29 @@ const Pool = require('pg').Pool;
 let myPort = process.env.PORT;
 let env = process.env.NODE_ENV;
 
-
+let pool;
 if(process.env.DATABASE_URL != undefined){
     let myconnectionString = {
         connectionString: process.env.DATABASE_URL,
         ssl: true
     };
     console.log("Checking db");
-    let pool = new Pool(myconnectionString);
+    pool = new Pool(myconnectionString);
     console.log("After db we out here");
 }
 
-const createTable = () => {
-    pool.query('CREATE TABLE IF NOT EXISTS nflsalaries(rank varchar(4),name varchar(45),pos varchar(4),team varchar(4), salary varchar(15))', (error,results) =>{
+const createTable = (conn) => {
+    conn.query('CREATE TABLE IF NOT EXISTS nflsalaries(rank varchar(4),name varchar(45),pos varchar(4),team varchar(4), salary varchar(15))', (error,results) =>{
         if(error){
             console.log("There was a Error in CreateTable");
             throw error;
         }
         console.log("WE HERE in create Table!!!!!!!!!");
-        //console.log(results);
+        console.log(results);
         //response.status(200);
     })
 }
-createTable();
+createTable(pool);
 
 
 
